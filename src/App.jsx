59798336135b7f0c7715935b8739e705a4021839
Ashish_Dubey <ashish95dubey@gmail.com>
2025-04-12 16:11:32 +0530

    React js and tailwind css installed , 1st template created

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-red-500'>Real_State_Marketplace</h1>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// // import reactLogo from './assets/react.svg'
+// // import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+// console.log('hello ashish'); // console ninja is not working
+
+//   return (
+//     <>
+//     <h2>
+//       Hello Ashish
+//     </h2>
+      
+//       <h1>Let's start with Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
